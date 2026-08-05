6395281bfd471068e674edf0cf4229c0a5dd308e
Gonzales-paaa <j2339@matsue-ct.ac.jp>
2026-08-05 05:39:45 +0000

    chap14失敗たすけて

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const knex = require('../db/knex');
 const mysql = require('mysql');
 
 const connection = mysql.createConnection({
@@ -12,10 +13,21 @@ const connection = mysql.createConnection({
 let todos = [];
 
 router.get('/', function (req, res, next) {
-  res.render('index', {
-    title: 'ToDo App',
-    todos: todos,
-  });
+  knex("tasks")
+    .select("*")
+    .then(function (results) {
+      console.log(results);
+      res.render('index', {
+        title: 'ToDo App',
+        todos: results,
+      });
+    })
+    .catch(function (err) {
+      console.error(err);
+      res.render('index', {
+        title: 'ToDo App',
+      });
+    });
 });
 
 router.post('/', function (req, res, next) {
