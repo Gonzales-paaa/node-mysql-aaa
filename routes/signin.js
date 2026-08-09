4fec38883ea20005299ce379bd6759cb937eee40
Gonzales-paaa <j2339@matsue-ct.ac.jp>
2026-08-09 15:33:48 +0000

    chap18passport認証の動作確認をしようから

--- routes/signin.js
+++ routes/signin.js
@@ -1,82 +1,21 @@
 const express = require('express');
 const router = express.Router();
-const knex = require('../db/knex');
-const bcrypt = require('bcrypt');
+const passport = require("passport");
 
 router.get('/', function (req, res, next) {
   const userId = req.session.userid;
   const isAuth = Boolean(userId);
-
   res.render("signin", {
     title: "Sign in",
     isAuth: isAuth,
   });
 });
 
-router.post('/', function (req, res, next) {
-  const userId = req.session.userid;
-  const isAuth = Boolean(userId);
-
-  const username = req.body.username;
-  const password = req.body.password;
-
-  knex("users")
-    .where({
-      name: username,
-    })
-    .select("*")
-    .then(function (results) {
-
-      if (results.length === 0) {
-
-        res.render("signin", {
-          title: "Sign in",
-          errorMessage: ["ユーザが見つかりません"],
-          isAuth: isAuth,
-        });
-
-      } else {
-
-        bcrypt.compare(password, results[0].password)
-          .then(function (comparedPassword) {
-
-            if (comparedPassword) {
-
-              req.session.userid = results[0].id;
-              res.redirect('/');
-
-            } else {
-
-              res.render("signin", {
-                title: "Sign in",
-                errorMessage: ["パスワードが違います"],
-                isAuth: isAuth,
-              });
-
-            }
-
-          })
-          .catch(function (err) {
-            console.error(err);
-
-            res.render("signin", {
-              title: "Sign in",
-              errorMessage: [err.message],
-              isAuth: isAuth,
-            });
-          });
-      }
-
-    })
-    .catch(function (err) {
-      console.error(err);
-
-      res.render("signin", {
-        title: "Sign in",
-        errorMessage: [err.sqlMessage],
-        isAuth: isAuth,
-      });
-    });
-});
+router.post('/', passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/signin',
+    failureFlash: true,
+  }
+));
 
 module.exports = router;
